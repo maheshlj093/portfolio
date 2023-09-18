05d72a21ce8976ef30d0ba980240f882280d8bf1
Mahesh <130858827+maheshlj093@users.noreply.github.com>
2023-09-18 13:52:49 +0530

    Update About.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -16,28 +16,30 @@ function About() {
           <div className="row mt-5">
             <div className="col-12">
               <p className='font-normal'>
-    Hello, I'm [Your Name], a passionate and driven software engineer with 1.6 years of hands-on experience in the ever-evolving world of web development. I thrive on turning ideas into innovative solutions and crafting digital experiences that leave a lasting impact.
+    Hello, I'm Mahesh, a passionate and driven software engineer with 1.6 years of hands-on experience in the ever-evolving world of web development. I thrive on turning ideas into innovative solutions and crafting digital experiences that leave a lasting impact.
 
-My Expertise
+<b>My Expertise</b>
 
-Languages: PHP, HTML, CSS, JavaScript
-Frameworks: AngularJS 1.0, jQuery
-Frontend: HTML5, CSS3, Bootstrap, Materialize CSS
-Backend: PHP, MySQL
-Web Technologies: AJAX, API Creation
-What I Do
+Languages: PHP, HTML, CSS, JavaScript<br>
+Frameworks: AngularJS 1.0, jQuery<br>
+Frontend: HTML5, CSS3, Bootstrap, Materialize CSS<br>
+Backend: PHP, MySQL<br>
+Web Technologies: AJAX, API Creation<br>
+  
+<b>What I Do</b>
 
 I'm currently working with a focus on:
 
-PHP: Building robust server-side applications and dynamic websites.
-HTML, CSS, and JavaScript: Crafting responsive and user-friendly frontend interfaces.
-AngularJS 1.0: Creating dynamic and data-driven web applications.
-jQuery: Enhancing user interactions and functionality.
-MySQL: Designing efficient databases to store and retrieve data.
-Bootstrap and Materialize CSS: Ensuring sleek and modern UI/UX.
-API Creation: Developing APIs for seamless data exchange.
-AJAX: Enabling asynchronous communication for a smoother user experience.
-Why Me?
+PHP: Building robust server-side applications and dynamic websites.<br>
+HTML, CSS, and JavaScript: Crafting responsive and user-friendly frontend interfaces.<br>
+AngularJS 1.0: Creating dynamic and data-driven web applications.<br>
+jQuery: Enhancing user interactions and functionality.<br>
+MySQL: Designing efficient databases to store and retrieve data.<br>
+Bootstrap and Materialize CSS: Ensuring sleek and modern UI/UX.<br>
+API Creation: Developing APIs for seamless data exchange.<br>
+AJAX: Enabling asynchronous communication for a smoother user experience.<br>
+  
+<b>Why Me?</b>
 
 With my passion for coding, attention to detail, and commitment to staying up-to-date with the latest trends and technologies, I am dedicated to delivering high-quality solutions that exceed expectations. Whether it's building a responsive website, optimizing database performance, or creating a seamless user experience, I'm always up for the challenge.
     </p>
