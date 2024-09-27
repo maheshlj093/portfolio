c2eaae43e67b7203c541726422941b7d697d03cc
Mahesh <130858827+maheshlj093@users.noreply.github.com>
2024-09-28 00:35:51 +0530

    Update Project.js

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -14,14 +14,9 @@ function Project() {
     <div className="row mt-5 d-flex justify-content-center">
       <div className="col-lg-4">
         <div className="list-group text-center">
-          <a href="https://techsparsha.in/dev/bml/" className="list-group-item list-group-item-action bg-danger text-white">BookMyLearn</a>
-          <a href="https://techsparsha.in/dev/MCC/" className="list-group-item list-group-item-action bg-primary text-white">MCC</a>
-          <a href="https://techsparsha.in/dev/dkp/" className="list-group-item list-group-item-action bg-info text-white">DKP</a>
-          <a href="https://techsparsha.in/dev/SVE/" className="list-group-item list-group-item-action bg-warning text-white">SolveEasy</a>
-          <a href="https://techsparsha.in/dev/TS/TechSparsha3/" className="list-group-item list-group-item-action bg-secondary text-white">TECHSPARSHA</a>
-          <a href="https://techsparsha.in/dev/ASDsite/" className="list-group-item list-group-item-action bg-danger text-white">ASD</a>
-          <a href="https://techsparsha.in/dev/CiMi" className="list-group-item list-group-item-action bg-danger text-white">CiMi</a>
-          <a href="https://techsparsha.in/dev/CiMi/field" className="list-group-item list-group-item-action bg-danger text-white">Smart EMS</a>
+        <a href="https://smartems.in" className="list-group-item list-group-item-action bg-danger text-white">SmartEMS</a>
+          <a href="https://www.dilipkpandey.in" className="list-group-item list-group-item-action bg-info text-white">DKP</a>
+          <a href="https://karnataka.aamaadmiparty.org" className="list-group-item list-group-item-action bg-warning text-white">AAP Karnataka</a>
         </div>
       </div>
     </div>
